dfe8d79866de28f5b25f47f6d057db81ae62ddad
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2022-12-18 17:09:33 +0800

    yes

--- routes/home.js
+++ routes/home.js
@@ -44,7 +44,7 @@ router.get('/username', (req, res) => {
 })
 
 
-router.put('/updateGoal/:userId', (req, res) => {
+router.put('/updateGoal', (req, res) => {
   goalDB.updateGoal(req.query.userId, req.body.goal, (err, result) => {
     if(err?.code === "ER_BAD_NULL_ERROR"){
       res.status(400).send({"error msg" : "goal cannot be null"});
@@ -52,6 +52,7 @@ router.put('/updateGoal/:userId', (req, res) => {
       console.error(err);
       res.status(500).send({"error msg" : "internal server error"});
     }else if(result.affectedRows === 0){
+      console.log(req.body)
       res.status(204).send();
     }else{
       res.status(201).send({"success msg" : "goal updated successfully"});
