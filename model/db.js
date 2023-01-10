bbca129c54ce250b51e7441337c1df7f997263e9
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2023-01-10 18:43:46 +0800

    yrs

--- model/db.js
+++ model/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 var db = {
   getConnection : function(){
     var dbConn = mysql.createConnection({
-      host: 'localhost', // Replace with your host name
-      user: 'sip_root',      // Replace with your database username
-      password: 'SipRootUser@2000',      // Replace with your database password
-      database: 'sip_db' // // Replace with your database Name
+      host: 'sql6.freemysqlhosting.net', // Replace with your host name
+      user: 'sql6589548',      // Replace with your database username
+      password: 'y33vwq7Jmg', // Replace with your database password
+      database: 'sql6589548' // // Replace with your database Name
     });
     return dbConn;
   }
